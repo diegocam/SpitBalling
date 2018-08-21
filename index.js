70590e2ec01c2b7cfb860e200e63a7177e3aab72
Diego Camacho <camachojcd@gmail.com>
2018-08-21 08:35:20 -0700

    changes

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const port = 8080;
 app.use(bodyParser.json());
 
 const verifyGitHub = (req) => {
-    console.log(req);
     // From Github and of PR event
     if (!req.headers['user-agent'].includes('GitHub-Hookshot') && req.headers['x-github-event'] !== 'pull_request') {
         return false;
@@ -19,7 +18,7 @@ const verifyGitHub = (req) => {
     // (hmac = hash-based message authentication code)
     const theirSignature = req.headers['x-hub-signature'];
     const payload = JSON.stringify(req.body);
-    const secret = 'SOME_TOKEN'; // TODO: Replace me
+    const secret = 'secret'; // TODO: Replace me
 
     const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
     console.log(ourSignature);
