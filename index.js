b9f111ba1d1d72e1849e12e832dc88a1c7f4fe4e
Diego Camacho <camachojcd@gmail.com>
2018-08-21 07:23:56 -0700

    Add body parser

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // -- Dependencies
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 8080;
 
+app.use(bodyParser.json());
 
 // Github endpoint. Used for PR Merges
 app.post('/pr-merge', (req, res) => {
     console.log(req);
+    console.log(res);
     res.status(200).end();
 });
 
