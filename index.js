995e41da6578ee458deb68b5bc70f6edc6fad6f5
Diego Camacho <camachojcd@gmail.com>
2018-08-21 07:51:47 -0700

    Add header reader

--- index.js
+++ index.js
@@ -7,13 +7,28 @@ const port = 8080;
 
 app.use(bodyParser.json());
 
+const verifyGitHub = (req) => {
+    console.log(req);
+    if (!req.headers['user-agent'].includes('GitHub-Hookshot')) {
+        return false;
+    }
+    // // Compare their hmac signature to our hmac signature
+    // // (hmac = hash-based message authentication code)
+    // const theirSignature = req.headers['x-hub-signature'];
+    // const payload = JSON.stringify(req.body);
+    // const secret = 'SOME_TOKEN'; // TODO: Replace me
+    // const ourSignature = `sha1=${crypto.createHmac('sha1', secret).update(payload).digest('hex')}`;
+    // return crypto.timingSafeEqual(Buffer.from(theirSignature), Buffer.from(ourSignature));
+};
+
 // Github endpoint. Used for PR Merges. Another Tests
 app.post('/pr-merge', (req, res) => {
-    // console.log(req.body);
-    console.log(req.body);
-    res.status(200).end();
+    if (verifyGitHub(req)) {
+        console.log(req.body);
+        res.status(200).end();
+    }
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
     console.log('bot running on ' + port);
 });
